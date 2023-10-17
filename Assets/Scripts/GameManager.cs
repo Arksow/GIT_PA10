79f92ecb32cfa7cf41b9c8c3d7cae738bdbee421
Arksow <alvinchan8303@gmail.com>
2023-10-17 16:15:39 +0800

    Commit Unity Project
    By Alvin Chan

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -38,8 +39,15 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        Time.timeScale = 0;
-        Txt_Message.text = "GAMEOVER! \nPRESS ENTER TO RESTART GAME.";
-        Txt_Message.color = Color.red;
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Score = 0;
+        Time.timeScale = 1;
+        Txt_Message.text = "";
+        Txt_Score.text = "SCORE : 0";
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+
+    float flyForce = 5f; // Adjust this value to control the upward flying force.
+    float deathHeight = -5f;
+    float maxHeight = 3.5f;
+    Rigidbody rb;
+
+    [SerializeField] GameObject duck;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y < 5f)
+        {
             thisAnimation.Play();
+            FlyUpwards();
+        }
+
+        if (transform.position.y < deathHeight)
+        {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            GameManager.thisManager.GameOver();
+        }
+    }
+
+    bool triggeredPoints;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PointTrigger") && !triggeredPoints)
+        {
+            triggeredPoints = true;
+            GameManager.thisManager.UpdateScore(1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PointTrigger") && triggeredPoints)
+        {
+            triggeredPoints = false;
+        }
+    }
+
+    void FlyUpwards()
+    {
+        rb.AddForce(Vector3.up * flyForce, ForceMode.Impulse);
+    }
+
+    private void Die()
+    {
+        GameManager.thisManager.GameOver();
     }
 }
